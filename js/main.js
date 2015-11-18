0a4fe016bd4aec3886baeac452e6c1322564ee0b
Henrique Garcês <hicke13@gmail.com>
2015-11-18 01:36:49 +0000

    first commit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,83 @@
 var map;
+var center = ol.proj.transform([-7.850000, 39.650761], 'EPSG:4326', 'EPSG:3857');
+var styles = [
+  'Road',
+  'Aerial',
+  'AerialWithLabels',
+];
+var controls = [
+	new ol.control.ScaleLine(),
+	new ol.control.MousePosition ({
+			coordinateFormat: ol.coordinate.createStringXY(6),
+			projection: 'EPSG:4326',
+			className: 'custom-mouse-position',
+	  		target: document.getElementById('mouse-position'),
+	  		undefinedHTML: '&nbsp;'
+	})
+
+];
 
 function init(){
-    map = new ol.Map({
+
+    /////////////////////
+    // LAYERS
+    /////////////////////
+    var osmLayer = new ol.layer.Tile({
+    	source: new ol.source.MapQuest({layer: 'osm'})
+	});
+	//map.addLayer(osmLayer);
+
+	var bingMapsLayers = [];
+	var i, ii;
+	for (i = 0, ii = styles.length; i < ii; ++i) {
+ 		bingMapsLayers.push(new ol.layer.Tile({
+	    	visible: false,
+	    	preload: Infinity,
+	    	source: new ol.source.BingMaps({
+	    		key: 'Ak-dzM4wZjSqTlzveKz5u0d4IQ4bRzVI309GxmkgSVr1ewS6iPSrOvOKhA-CJlm3',
+	     		imagerySet: styles[i],
+	      		maxZoom: 19
+	    	})
+  		}));
+	}
+
+	/////////////////////
+	// MAP/VIEW
+	/////////////////////
+	map = new ol.Map({
         target:'map',
         renderer:'canvas',
+        layers: bingMapsLayers,
+        controls: controls,
     	view: new ol.View({
     		projection: 'EPSG:900913',
-    		center:[-1017759.03868,4684061.08394],
+    		center: center,
     		zoom:7
-    	})   
+    	})
     });
 
-    ////////////
-    // LAYERS
-    ////////////
-    var osmLayer = new ol.layer.Tile({
-    	source: new ol.source.OSM()
+	/////////////////////
+	// MOUSE EVENTS
+	/////////////////////
+	map.on('click', function(event) {
+		var coord = event.coordinate;
+		var degrees = ol.proj.transform(coord, 'EPSG:3857', 'EPSG:4326');
+		var hdms = ol.coordinate.toStringHDMS(degrees);
+		var element = overlay.getElement();
+
 	});
-	map.addLayer(osmLayer);
+
+	var select = document.getElementById('layer-select');
+	function onChange() {
+		var style = select.value;
+  		for (var i = 0, ii = bingMapsLayers.length; i < ii; ++i) {
+    		bingMapsLayers[i].setVisible(styles[i] === style);
+  		}
+	}
+	select.addEventListener('change', onChange);
+	onChange();
+	
 }
+
 
 
